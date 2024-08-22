07aef24161e69e31ec0fe2791e3c2f6fe3dab2a8
ellielle <40385743+ellielle@users.noreply.github.com>
2024-08-22 07:14:57 -0400

    add fps clock

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ def main():
     print("Starting asteroids!")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    # delta time
+    dt = 0
 
     while True:
         # Allows the close button to close the game and not hang
@@ -15,6 +18,10 @@ def main():
                 return
         screen.fill("#000000")
         pygame.display.flip()
+        # set the fps to 60
+        # get the delta time from the last frame
+        # by dividing the value returned from tick by 1000
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
